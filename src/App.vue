67d99c64d8dad5e941eb1a821e542ba73a73fa3f
yushirui <2514904609@qq.com>
2020-09-21 19:17:52 +0800

    feat:初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hi</div>
+  <Yushirui />
+</template>
+
+<script>
+import Yushirui from "./components/Yushirui.vue";
+
+export default {
+  name: "App",
+  components: {
+    Yushirui,
+  },
+};
+</script>
