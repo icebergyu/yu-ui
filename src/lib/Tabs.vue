4e2b21860edd43f4775440ee9a81daad633cf1bd
yushirui <2514904609@qq.com>
2020-10-05 13:46:24 +0800

    feat:渲染Tabs内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,11 +1,24 @@
 <template>
-  <div>Tabs</div>
+  <div>
+    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
+    <component v-for="(c, index) in defaults" :is="c" :key="index" />
+  </div>
 </template>
 
 <script>
+import Tab from "./Tab.vue";
 export default {
-  setup() {
-    return {};
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs的子标签必须是Tab");
+      }
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    return { defaults, titles };
   },
 };
 </script>
