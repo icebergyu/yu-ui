77b191ee57a7d6a99526b6c1d75635c995ed4187
yushirui <2514904609@qq.com>
2020-10-05 14:14:44 +0800

    feat:用css样式实现切换tab

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,38 @@
 <template>
   <div>
-    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index" />
+    <div class="yu-tabs">
+      <div class="yu-tabs-nav">
+        <div
+          class="yu-tabs-nav-item"
+          v-for="(t, index) in titles"
+          :key="index"
+          @click="select(t)"
+          :class="{ selected: t === selected }"
+        >
+          {{ t }}
+        </div>
+      </div>
+      <div class="yu-tabs-content">
+        <component
+          class="yu-tabs-content-item"
+          v-for="(c, index) in defaults"
+          :is="c"
+          :class="{ selected: c.props.title === selected }"
+          :key="index"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Tab from "./Tab.vue";
 export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, context) {
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
@@ -18,10 +43,43 @@ export default {
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
-    return { defaults, titles };
+    const select = (title: string) => {
+      context.emit("update:selected", title);
+    };
+    return { defaults, titles, select };
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.yu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
 </style>
